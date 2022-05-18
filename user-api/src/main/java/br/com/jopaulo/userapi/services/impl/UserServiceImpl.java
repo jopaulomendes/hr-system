2e013e4a7cf48758b3f95cf63f8e3bb3ba9ffae3
João Paulo <jp_cbc@hotmail.com>
2022-05-18 00:11:54 -0300

    Load Balancing

--- user-api/src/main/java/br/com/jopaulo/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/jopaulo/userapi/services/impl/UserServiceImpl.java
@@ -2,23 +2,27 @@ package br.com.jopaulo.userapi.services.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import br.com.jopaulo.userapi.domain.User;
 import br.com.jopaulo.userapi.repository.UserRepository;
 import br.com.jopaulo.userapi.services.UserService;
 import br.com.jopaulo.userapi.services.exceptions.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService{
 
-	@Autowired
-	private UserRepository repository;
+	private final UserRepository repository;
+	private final Environment env;
 
 	@Override
 	public User findById(Long id) {
+		log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
 		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 
